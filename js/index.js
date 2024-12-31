bce0c46204122261baf9232d0a22297cfcd958bc
2AhmedHesham9 <ahmedzaitoon4@gmail.com>
2024-12-31 22:00:55 +0200

    add all projects and refactor design

--- js/index.js
+++ js/index.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal('.home-content p,.about-content', { origin: 'right' });
 
 const typed = new Typed('.multiple-text', {
 
-    strings: ['Frontend Developer', 'Backend Developer', 'Desktop Developer C#'],
+    strings: ['Backend Developer','Frontend Developer',  'Computer Science Student'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
